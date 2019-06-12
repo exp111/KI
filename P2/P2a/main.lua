847e6526a69ee66c5bd2c00dd4c1ee09a42a5457
Exp <theexp111@gmail.com>
2019-06-12 16:38:44 +0200

    P2a

---
+++ P2/P2a/main.lua
@@ -0,0 +1,42 @@
+dofile("include/Queens.lua")
+
+queens = nil
+function love.load()
+    queens = Queens:new({1,1,1,1,1,1,1,1})
+    queens:checkHits()
+    size = #queens.grid
+    boxSize = 100
+    queenImg = love.graphics.newImage("Queen.png")
+    queenImgFactor = boxSize / queenImg:getHeight()
+    love.window.setMode(size * boxSize, size * boxSize, {})
+end
+
+function love.draw()
+    for x = 1, size do
+        for y = 1, size do
+            if (x + y) % 2 == 0 then
+                love.graphics.setColor(1, 1, 1)
+            else
+                love.graphics.setColor(0.741, 0.765, 0.78)
+            end
+            love.graphics.rectangle("fill", (x - 1) * boxSize, (y - 1) * boxSize, boxSize, boxSize)
+            if queens.grid[x][y].queen == 1 then
+                love.graphics.draw(queenImg, (x - 1) * boxSize, (y - 1) * boxSize, 0, queenImgFactor)
+            end
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.print(queens.grid[x][y].h, (x - 1) * boxSize, (y - 1) * boxSize)
+        end
+    end
+end
+
+function love.update()
+end
+
+function love.keypressed(key)
+    if key == 'a' then
+        queens:action({x = 1, y = 1}, {x = 1, y = 5})
+    end
+    if key == 's' then
+        queens:action({x = 2, y = 1}, {x = 1, y = 6})
+    end
+end
---
+++ P2/include/Queens.lua
@@ -0,0 +1,77 @@
+dofile("include/Class.lua")
+
+Field = {}
+local Field_mt = Class(Field)
+
+function Field:new(queen)
+    return setmetatable({queen = queen and 1 or 0, h = 0}, Field_mt)
+end
+
+Queens = {}
+local Queens_mt = Class(Queens)
+function Queens:new(initialState)
+    -- Init Grid
+    local grid = {}
+    for x = 1, 8 do
+        grid[x] = {}
+        for y = 1, 8 do
+            grid[x][y] = Field:new(initialState[x] == y)
+        end
+    end
+
+    return setmetatable({
+        initialState = initialState,
+        transisionModel = transition,
+        grid = grid
+    }, Queens_mt)
+end
+
+function Queens:hits(pos)
+    local count = 0
+    for i = 1, #self.grid do
+        --Horizontal
+        if pos.x ~= i and self.grid[i][pos.y].queen == 1 then
+            count = count + 1
+        end
+        --Vertical
+        if pos.y ~= i and self.grid[pos.x][i].queen == 1 then
+            count = count + 1
+        end
+
+        local diagX = i + pos.x - pos.y
+        --Diagonal from top left
+        if diagX > 0 and diagX <= #self.grid then
+            if pos.x ~= diagX and self.grid[diagX][i].queen == 1 then
+                count = count + 1
+            end
+        end
+        
+        --Diagonal from bottom left
+        local reverseDiagY = #self.grid - i + 1
+        local reverseDiagX = i + pos.x - (#self.grid - pos.y + 1)
+        if reverseDiagX > 0 and reverseDiagX <= #self.grid and reverseDiagY > 0 and reverseDiagY <= #self.grid then
+            if pos.x ~= reverseDiagX and self.grid[reverseDiagX][reverseDiagY].queen == 1 then
+                count = count + 1
+            end
+        end
+    end
+    return count
+end
+
+function Queens:checkHits()
+    for x = 1, #self.grid do
+        for y = 1, #self.grid do
+            self.grid[x][y].h = self:hits({x = x, y = y})
+        end
+    end
+end
+
+function Queens:transition(a,b)
+    self.grid[a.x][a.y].queen = 0
+    self.grid[b.x][b.y].queen = 1
+end
+
+function Queens:action(a,b)
+    self:transition(a,b)
+    self:checkHits()
+end
