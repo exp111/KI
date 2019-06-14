ec6d46ba6d15542dcdf2e9f2a45efc77cd8c4bc1
T N <theexp111@gmail.com>
2019-06-14 13:36:58 +0200

    -rEeEeE -finished p2c tho

--- P2/P2c/main.lua
+++ P2/P2c/main.lua
@@ -34,35 +34,14 @@ end
 function love.update()
 end
 
-function Backtrack(assignment, csp, i)
-    if #assignment == 8 then
-        return assignment
-    end
-
-    for j = 1, 8 do
-        table.insert(assignment, j)
-        --print("lvl: " .. #assignment .. " j: " .. j)
-        local queen = Queens:new2(assignment, 8)
-        queen:heuristic()
-        if queen.fit == 0 then
-            --print("lvl: " .. #assignment .. " j: " .. j)
-            local result = Backtrack(assignment, csp, i)
-            if result ~= false then
-                return assignment
-            end
-        end
-        table.remove(assignment)
-    end
-    return false
-end
-
 function love.keypressed(key)
     if key == 'a' then
-        local new = Backtrack({}, {}, 0)
-        if new ~= false then
-        queens = Queens:new(new)
+        local new = Backtracking(size)
+        if #new > 0 then
+        --queens = Queens:new(new)
         end
-        print(#solutions)
+        print(#new)
+        solutions = new
     end
     if key == 's' then
         currSolution = currSolution + 1
@@ -70,8 +49,43 @@ function love.keypressed(key)
             currSolution = #solutions > 0 and 1 or 0
         end
         if currSolution ~= 0 then
-            queens = solutions[currSolution]
+            queens = Queens:new(solutions[currSolution])
             print(currSolution)
         end
     end
+end
+
+function Backtracking(max)
+    local ret = Backtrack({{}}, max)
+    table.remove(ret, 1) -- last wip assignment is still in queue
+    return ret
+end
+
+function Backtrack(assignment, max)
+    local cur = assignment[1] -- current wip is always in front
+    if #cur == max then
+        local curCopy = copy(cur)
+        table.insert(assignment, curCopy)
+        return assignment
+    end
+
+    for j = 1, max do
+        local cur = assignment[1]
+        table.insert(cur, j) -- try new state
+        local queen = Queens:new2(cur, max)
+        queen:heuristic()
+        if queen.fit == 0 then -- valid
+            Backtrack(assignment, max) -- no need to add cuz everything is pointered
+        end
+        table.remove(cur) -- remove the state so we can do a new one -> if it was valid solution is already added else fuck it
+    end
+    return assignment
+end
+
+function copy(t)
+    local newT = {}
+    for k, v in pairs(t) do
+        newT[k] = v
+    end
+    return newT
 end
