158ac5a7a5d2a7d0b77f8ca45fdf64526e473962
T N <theexp111@gmail.com>
2019-06-15 20:13:57 +0200

    some smaller stuff

--- P3/P3a/main.lua
+++ P3/P3a/main.lua
@@ -1,16 +1,21 @@
 dofile("include/Wumpus.lua")
+function math.clamp(low, n, high) return math.min(math.max(n, low), high) end
 
 function love.load()
+    math.randomseed(os.time())
+
     wumpus = Wumpus:new({x = 1, y = 1}, 4)
     size = #wumpus.grid
     boxSize = 100
-    barSize = 50
+    barSize = 65
     love.window.setMode(size * boxSize, size * boxSize + barSize, {})
 
     playerImg = love.graphics.newImage("Player.png")
     playerImgFactor = (boxSize / 2) / playerImg:getHeight()
     playerImgWidth = playerImg:getWidth() / 2
     playerImgHeight = playerImg:getHeight() / 2
+
+    eventText = ""
 
     grey = {0.741, 0.765, 0.78}
     halfPi = math.pi / 2
@@ -45,31 +50,72 @@ function love.draw()
     local barPosY = size * boxSize
     love.graphics.setColor(0, 0, 0)
     love.graphics.print("Score: " .. wumpus.score, 10, barPosY)
-    love.graphics.print("Percept: " .. "TODO", 10, barPosY + 15) -- TODO: this
+    local percept = wumpus:getPercept(wumpus.player.pos, wumpus.player.rotation)
+    local perceptString = string.format("Percept: [%s %s %s %s %s] (Stench, Breeze, Glitter, Bump, Scream)", 
+        percept.stench == 1 and 'S' or 'N',
+        percept.breeze == 1 and 'B' or 'N',
+        percept.glitter == 1 and 'G' or 'N',
+        percept.bump == 1 and 'B' or 'N',
+        percept.scream == 1 and 'S' or 'N'
+    )
+    love.graphics.print(perceptString, 10, barPosY + 15) -- TODO: this
     love.graphics.print("Action: (F, L, R, G, S or C)?", 10, barPosY + 30)
+    love.graphics.print(eventText, 10, barPosY + 45)
 end
 
 function love.update()
 end
 
 function love.keypressed(key)
-    if key == 'l' then
+    wumpus:action(key)
+    
+    if key == 'l' then -- LEFT
         wumpus.player.rotation = wumpus.player.rotation - halfPi
         if wumpus.player.rotation < 0 then
             wumpus.player.rotation = wumpus.player.rotation + twoPi
         end
+        eventText = "Turned left"
     end
-    if key == 'r' then
+    if key == 'r' then -- RIGHT
         wumpus.player.rotation = wumpus.player.rotation + halfPi
         if wumpus.player.rotation > twoPi then
             wumpus.player.rotation = wumpus.player.rotation - twoPi
         end
+        eventText = "Turned right"
     end
-    if key == 'f' then
-        local delta = {}
-        delta.x = ((wumpus.player.rotation / halfPi) % 2 == 0) and 0 or 1
-        delta.x = wumpus.player.rotation > halfPi and -1 or 1
-        delta.y = 0
-        wumpus:move({x = wumpus.player.pos.x + delta.x, y = wumpus.player.pos.y + delta.y})
+    if key == 'f' then -- FORWARD
+        local delta = wumpus:getDelta(wumpus.player.rotation)
+        local newPos = {x = wumpus.player.pos.x + delta.x, y = wumpus.player.pos.y + delta.y}
+        if newPos.x >= 1 and newPos.x <= size and newPos.y >= 1 and newPos.y <= size then
+            local result = wumpus:move(newPos)
+            if result == 0 then
+                eventText = "Moved forward"
+            end
+            if result == 1 then
+                eventText = "The wumpus ate you"
+                wumpus:action('w')
+            end
+            if result == 2 then
+                eventText = "You fell down a pit"
+                wumpus:action('p')
+            end
+        else
+            eventText = "Can't move"
+        end
+    end
+    if key == 'g' then -- GRAB
+        if wumpus.grid[wumpus.player.pos.x][wumpus.player.pos.y].gold == 1 then
+            eventText = "Grabbed Gold"
+            wumpus.player.hasGold = 1
+            wumpus.grid[wumpus.player.pos.x][wumpus.player.pos.y].gold = 0
+        else
+            eventText = "No Gold"
+        end
+    end
+    if key == 's' then -- TODO: SHOOT
+        eventText = "Shot Arrow"
+    end
+    if key == 'c' then -- TODO: c
+        
     end
 end
--- P3/include/Wumpus.lua
+++ P3/include/Wumpus.lua
@@ -3,12 +3,15 @@ dofile("include/Class.lua")
 Field = {}
 local Field_mt = Class(Field)
 
-function Field:new(visited)
+function Field:new(visited, pit)
     return setmetatable({
         visited = visited and 1 or 0,
         stench = 0,
         breeze = 0,
         glitter = 0,
+        gold = 0,
+        pit = pit,
+        wumpus = 0
         }, Field_mt)
 end
 
@@ -18,7 +21,8 @@ local Player_mt = Class(Player)
 function Player:new(pos)
     return setmetatable({
         pos = pos,
-        rotation = 0
+        rotation = 0,
+        hasGold = 0
         }, Field_mt)
 end
 
@@ -28,22 +32,99 @@ local Wumpus_mt = Class(Wumpus)
 function Wumpus:new(startPos, size)
     -- Init Grid
     local grid = {}
+    local hasGold = 0
+    local hasWumpus = 0
     for x = 1, size do
         grid[x] = {}
         for y = 1, size do
             local isStartPos = startPos.x == x and startPos.y == y
-            grid[x][y] = Field:new(isStartPos)
+            local isPit = x ~= 1 and y ~= 1 and math.random(10) <= 2 and 1 or 0
+            grid[x][y] = Field:new(isStartPos, isPit)
         end
     end
+
+    -- Gold
+    local gold = {x = math.random(size), y = math.random(size)}
+    grid[gold.x][gold.y].gold = 1
+
+    local wumpus = {x= math.random(size), y = math.random(size)}
+    grid[wumpus.x][wumpus.y].wumpus = 1
+
+    local scoreBoard = {
+        f = 1,
+        l = 1,
+        r = 1,
+        g = 1,
+        s = 11,
+        c = 1,
+        p = 1000,
+        w = 1000
+    }
 
     return setmetatable({
         grid = grid,
         score = 0,
-        player = Player:new(startPos)
+        player = Player:new(startPos),
+        scoreBoard = scoreBoard
     }, Wumpus_mt)
 end
 
 function Wumpus:move(pos)
     self.player.pos = pos
     self.grid[pos.x][pos.y].visited = 1
+    if self.grid[pos.x][pos.y].wumpus == 1 then
+        return 1
+    end
+    if self.grid[pos.x][pos.y].pit == 1 then
+        return 2
+    end
+    return 0 -- 0 if ok, 1 if wumpus, 2 if pit
+end
+
+function Wumpus:action(key)
+    self.score = self.score - (self.scoreBoard[key] or 0)
+end
+
+function Wumpus:getDelta(rotation)
+    local delta = {}
+    delta.x = ((rotation / halfPi) % 2 == 0) and 0 or 1 --remove y
+    delta.y = delta.x == 0 and 1 or 0 --remove x
+    delta.x = delta.x == 1 and (rotation % twoPi > halfPi and -1 or 1) or 0
+    delta.y = delta.y == 1 and (rotation % twoPi == 0 and -1 or 1) or 0
+    return delta
+end
+
+function Wumpus:getPercept(pos, rotation)
+    local percept = {breeze = 0, stench = 0, glitter = 0, bump = 0, scream = 0}
+    if pos.x < 1 or pos.x > #self.grid or pos.y < 1 or pos.y > #self.grid then --invalid pos
+        return percept
+    end
+    for i = -1, 1, 2 do
+        local newPos = {x = pos.x + i, y = pos.y + i}
+        if newPos.x >= 1 and newPos.x <= #self.grid then
+            if self.grid[newPos.x][pos.y].wumpus == 1 then
+                percept.stench = 1
+            end
+            if self.grid[newPos.x][pos.y].pit == 1 then
+                percept.breeze = 1
+            end
+        end
+
+        if newPos.y >= 1 and newPos.y <= #self.grid then
+            if self.grid[pos.x][newPos.y].wumpus == 1 then
+                percept.stench = 1
+            end
+            if self.grid[pos.x][newPos.y].pit == 1 then
+                percept.breeze = 1
+            end
+        end
+    end
+    percept.glitter = self.grid[pos.x][pos.y].gold == 1 and 1 or 0
+    percept.scream = self.grid[pos.x][pos.y].wumpus == 1 and 1 or 0
+    local delta = self.getDelta(pos, rotation)
+    local newPos = {x = pos.x + delta.x, y = pos.y + delta.y}
+    if newPos.x < 1 or newPos.x > size or newPos.y < 1 or newPos.y > size then --OOB
+        percept.bump = 1
+    end
+    return percept
 end
